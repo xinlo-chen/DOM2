6dc60c5aa599b8d3135bf17c5eaccc31b5f78216
Jay <1311257306@qq.com>
2021-11-05 22:36:37 +0800

    更新

--- dist/dom.1d0b6d56.js
+++ dist/dom.1d0b6d56.js
@@ -119,70 +119,8 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"dom.js":[function(require,module,exports) {
 window.dom = {
-  creat: function creat(string) {
-    var container = document.createElement("template");
-    container.innerHTML = string.trim();
-    return container.content.firstChild;
-  },
-  after: function after(node, node2) {
-    console.log(node.nextSibling);
-    node.parentNode.insertBefore(node2, node.nextSibling);
-  },
-  before: function before(node, node2) {
-    node.parentNode.insertBefore(node2, node);
-  },
-  append: function append(parent, node) {
-    parent.appendChild(node);
-  },
-  wrap: function wrap(node, parent) {
-    dom.before(node, parent);
-    dom.append(parent, node);
-  },
-  remove: function remove(node) {
-    node.parentNode.removeChild(node);
-    return node;
-  },
-  empty: function empty(node) {
-    var childNodes = node.childNodes;
-    var array = [];
-    var x = node.firstChild;
-
-    while (x) {
-      array.push(dom.remove(node.firstChild));
-      x = node.firstChild;
-    }
-
-    return array;
-  },
-  attr: function attr(node, name, value) {
-    //重载
-    if (arguments.length === 3) {
-      node.setAttribute(name, value);
-    } else if (arguments.length === 2) {
-      return node.getAttribute(name);
-    }
-  },
-  text: function text(node, string) {
-    if (arguments.length === 2) {
-      if ('innerText' in node) {
-        node.innerText = string;
-      } else {
-        node.textContent = string;
-      }
-    } else if (arguments.length === 1) {
-      if ('innerText' in node) {
-        return node.innerText;
-      } else {
-        return node.textContent;
-      }
-    }
-  },
-  html: function html(node, string) {
-    if (arguments.length === 2) {
-      node.innerHTML = string;
-    } else if (arguments.length === 1) {
-      return node.innerHTML;
-    }
+  find: function find(selector, scope) {
+    return (scope || document).querySelectorAll(selector);
   },
   style: function style(node, name, value) {
     if (arguments.length === 3) {
@@ -202,71 +140,10 @@ window.dom = {
       }
     }
   },
-  class: {
-    add: function add(node, className) {
-      node.classList.add(className);
-    },
-    remove: function remove(node, className) {
-      node.classList.remove(className);
-    },
-    has: function has(node, className) {
-      return node.classList.contains(className);
-    }
-  },
-  on: function on(node, eventName, fn) {
-    node.addEventListener(eventName, fn);
-  },
-  off: function off(node, eventName, fn) {
-    node.removeEventListener(eventName, fn);
-  },
-  find: function find(selector, scope) {
-    return (scope || document).querySelectorAll(selector);
-  },
-  parent: function parent(node) {
-    return node.parentNode;
-  },
-  children: function children(node) {
-    return node.children;
-  },
-  siblings: function siblings(node) {
-    return Array.from(node.parentNode.children).filter(function (n) {
-      return n !== node;
-    });
-  },
-  next: function next(node) {
-    var x = node.nextSibling;
-
-    while (x && x.nodeType === 3) {
-      x = x.nextSibling;
-    }
-
-    return x;
-  },
-  previous: function previous(node) {
-    var x = node.previousSibling;
-
-    while (x && x.nodeType === 3) {
-      x = x.previousSibling;
-    }
-
-    return x;
-  },
   each: function each(nodeList, fn) {
     for (var i = 0; i < nodeList.length; i++) {
       fn.call(null, nodeList[i]);
     }
-  },
-  index: function index(node) {
-    var list = dom.children(node.parentNode);
-    var i;
-
-    for (i = 0; i < list.length; i++) {
-      if (list[i] === node) {
-        break;
-      }
-    }
-
-    return i;
   }
 };
 },{}],"C:/Users/Chenyilong/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -297,7 +174,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59585" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57979" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
